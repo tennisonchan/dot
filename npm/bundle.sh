4e0c6b7509f55be49f797e8410d351ef80e2ed22
Tennison Chan <tennison.chan@gmail.com>
2016-04-04 16:16:30 -0400

    Rename to bundle, install and init instead

--- npm/bundle.sh
+++ npm/bundle.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 #
-# .npmfile:
+# .npmfile
 #   Stores all npm global packages with responding versions
-# sync.sh:
+# init.sh
+#   To make sure npm or node or iojs was installed
+# install.sh
 #   Sync the npm global packages based on .npmfile, regardless if .npmfiles is the latest version or not.
 #   Will not remove installed global packages which is not on .npmfile.
-# update.sh:
+# update.sh
 #   Update the .npmfile based on npm global packages on the local machine.
 #   Add and commit .npmfile.
 
 NPMFILE_PATH="$(pwd)/.npmfile"
 
+sh init.sh
+
 if [ -f $NPMFILE_PATH ]; then
-  sh sync.sh
+  sh install.sh
 then
   sh update.sh
-  sh sync.sh
+  sh install.sh
 fi;
---
+++ npm/init.sh
@@ -0,0 +1,5 @@
+# !/bin/sh
+#
+#    To make sure npm or node or iojs was installed
+
+brew install node;
