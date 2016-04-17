63cd660311bb4857c5bd121be0612bf6c3398913
Tennison Chan <tennison.chan@gmail.com>
2016-04-16 23:59:49 -0400

    Modify dot symlink

--- Dot/cmd/symlink.sh
+++ Dot/cmd/symlink.sh
@@ -8,15 +8,15 @@ list_all_dotfiles () {
 }
 
 symlink_dotfiles () {
-  echo 'Symlinking dotfiles'
+  echo "Symlinking dotfiles"
 
-  local from_dir=$1 to_dir=$2
+  local src_dir=$1 dst_dir=$2
   local overwrite_all=false backup_all=false skip_all=false
 
-  pushd $from_dir > /dev/null;
-  
-  list_all_dotfiles $from_dir | while read src; do
-    dst="$to_dir/$(basename $src)"
+  pushd $src_dir > /dev/null;
+
+  list_all_dotfiles $src_dir | while read src; do
+    dst="$dst_dir/$(basename $src)"
     link_file $src $dst
   done
 
@@ -29,7 +29,7 @@ link_file () {
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
+  if [ -f $dst -o -d $dst -o -L $dst ]; then
 
     if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
       local currentSrc="$(readlink $dst)"
@@ -41,7 +41,7 @@ link_file () {
         [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
         read -n 1 action < /dev/tty;
 
-        case "$action" in
+        case $action in
           o )
             overwrite=true;;
           O )
@@ -64,25 +64,24 @@ link_file () {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]; then
-      rm -rf "$dst"
+    if [ $overwrite == "true" ]; then
+      rm -rf $dst
       echo "removed $dst"
     fi
 
-    if [ "$backup" == "true" ]; then
-      mv "$dst" "${dst}.backup"
+    if [ $backup == "true" ]; then
+      mv $dst "${dst}.backup"
       echo "moved $dst to ${dst}.backup"
     fi
 
-    if [ "$skip" == "true" ]; then
+    if [ $skip == "true" ]; then
       echo "skipped $src"
     fi
   fi
 
-  # "false" or empty
-  if [ "$skip" != "true" ]; then
-    ln -s "$1" "$2"
-    echo "linked $(basename $1) to HOME"
+  if [ $skip != "true" ]; then
+    ln -s $src $dst
+    echo "linked $(basename $src) to HOME"
   fi
 }
 
