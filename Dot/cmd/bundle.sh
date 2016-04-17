29d38fe5da4a2172dec63a6c8fa43bc67acc11a4
Tennison Chan <tennison.chan+gh@gmail.com>
2016-04-17 16:19:30 -0400

    Remove running bundle for all topics from dot-bundle; ask users for each topic when they run dot-init

--- Dot/cmd/bundle.sh
+++ Dot/cmd/bundle.sh
@@ -3,11 +3,6 @@
 bundle_on_topics () {
   if [[ -n $DOT_TOPIC ]]; then
     run_command "bundle" $DOT_TOPIC
-  else
-    all_topics | while read topic; do
-      set_topic "$topic"
-      run_command "bundle" $topic
-    done
   fi;
 }
 
--- Dot/cmd/init.sh
+++ Dot/cmd/init.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set +o posix
 
-init () {
+run_init_on_each_topic () {
   if [[ -n $DOT_TOPIC ]]; then
     run_command "init" $DOT_TOPIC;
   else
     all_topics | while read topic; do
-      run_command "init" $topic;
+      echo "Do you want to initialize for $topic? [y/n]"
+      read -n 1 answer < /dev/tty;
+      if [[ "$answer" == "y" ]]; then
+        run_command "init" $topic;
+      fi
     done
   fi;
 }
 
-init;
+run_init_on_each_topic;
 source "$DOT_DIRECTORY/cmd/bundle.sh";
