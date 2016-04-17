b9e8e5264c05525fe96dbc6f8cc6e6619eee6104
Tennison Chan <tennison-chan@Tennisons-MacBook-Air.local>
2016-04-17 00:32:05 -0400

    Remove backup logic on dot-symlink

--- Dot/cmd/symlink.sh
+++ Dot/cmd/symlink.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set +o posix
 
-echo "Dot/symlink"
-
 list_all_dotfiles () {
   find $1 -maxdepth 1 -mindepth 1 -name ".*"
 }
@@ -11,7 +9,7 @@ symlink_dotfiles () {
   echo "Symlinking dotfiles"
 
   local src_dir=$1 dst_dir=$2
-  local overwrite_all=false backup_all=false skip_all=false
+  local overwrite_all=false skip_all=false
 
   pushd $src_dir > /dev/null;
 
@@ -26,19 +24,19 @@ symlink_dotfiles () {
 link_file () {
   local src=$1 dst=$2
 
-  local overwrite= backup= skip=
+  local overwrite= skip=
   local action=
 
   if [ -f $dst -o -d $dst -o -L $dst ]; then
 
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
+    if [ "$overwrite_all" == "false" ] && [ "$skip_all" == "false" ]; then
       local currentSrc="$(readlink $dst)"
 
       if [ "$currentSrc" == "$src" ]; then
         skip=true;
       else
         echo "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
-        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all?"
         read -n 1 action < /dev/tty;
 
         case $action in
@@ -46,10 +44,6 @@ link_file () {
             overwrite=true;;
           O )
             overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
           s )
             skip=true;;
           S )
@@ -61,17 +55,11 @@ link_file () {
     fi
 
     overwrite=${overwrite:-$overwrite_all}
-    backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
     if [ $overwrite == "true" ]; then
       rm -rf $dst
       echo "removed $dst"
-    fi
-
-    if [ $backup == "true" ]; then
-      mv $dst "${dst}.backup"
-      echo "moved $dst to ${dst}.backup"
     fi
 
     if [ $skip == "true" ]; then
