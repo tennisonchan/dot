e2c756612b4f389e032b73e3e92eac6833fbd158
Tennison Chan <tennison.chan+gh@gmail.com>
2016-04-18 22:28:32 -0400

    Update npm install with extracting the packages with versions from npmfile

--- Topics/npm/install.sh
+++ Topics/npm/install.sh
@@ -3,15 +3,20 @@ set +o posix
 
 source "$DOT_TOPIC_DIRECTORY/share.sh";
 
+get_package_with_versions () {
+  cat $1 | grep "from" | awk -F "\"" '{print $4}'
+}
+
 install_npm_packages () {
-  if [[ -f $NPMFILE_PATH ]]; then
-    cat $NPMFILE_PATH | while read line; do
+  local npmfile=$1
+  if [[ -f $npmfile ]]; then
+    get_package_with_versions $npmfile | while read line; do
       echo "Installing npm global packages $line"
       npm install -g $line
     done
   else
-    echo "Missing file Npmfile. Please run 'dot update npm'."
+    echo "Missing file Npmfile. Please run 'dot update npm'"
   fi;
 }
 
-install_npm_packages
+install_npm_packages $NPMFILE_PATH
