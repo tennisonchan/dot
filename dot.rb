5ee4e1d2b71a1294dda358444ccca73c18545ff9
Tennison Chan <tennison@Tennisons-MacBook-Pro.local>
2016-10-12 18:39:24 -0400

    Update the sha256

--- dot.rb
+++ dot.rb
@@ -3,7 +3,7 @@ class Dot < Formula
   homepage "https://github.com/tennisonchan/dot"
   url "https://github.com/tennisonchan/dot/archive/0.1.1.tar.gz"
   version "0.1.1"
-  sha256 "833817c91723f5545e5ee6487807cc59d985c5950c41ddaacbb542eb3ad07fe8"
+  sha256 "25794f5721ecf9e01543236fd19123cc72c9f5cb2de769562b1f59ca13b3fa25"
 
   def install
     bin.install "bin/dot"
